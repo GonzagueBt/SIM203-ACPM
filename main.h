345778a45a4061c407847750e151f639d8c1ed60
pierre6398 <59994123+pierre6398@users.noreply.github.com>
2022-04-20 18:33:24 +0200

    rajout de network_size()

--- main.h
+++ main.h
@@ -2,5 +2,5 @@
 float distance(float v1lon,float v1lat, float v2lon, float v2lat);
 int* Prim(ListOfCities* cities);
 void citiesWriter(const char* write, int* voisin, int number);
+float network_size(ListOfCities* cities, int* voisin, int n);
 ListOfCities* maxbyDep(ListOfCities* cities);
-
